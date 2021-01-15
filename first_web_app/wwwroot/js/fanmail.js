2f25dc394bb30462c70f60b8de9141488f993efd
madisonc33 <61567086+madisonc33@users.noreply.github.com>
2021-01-14 20:13:52 -0700

    Initial check-in of module first_web_app

---
+++ first_web_app/wwwroot/js/fanmail.js
@@ -0,0 +1,8 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    alert("From = " + document.getElementById("from").value + "\n" +
+        "Email = " + document.getElementById("email").value + "\n" +
+        "Subject = " + document.getElementById("subject").value + "\n" +
+        "Message = " + document.getElementById("message").value)
+});
